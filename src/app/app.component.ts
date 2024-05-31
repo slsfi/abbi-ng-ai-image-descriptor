6afb74d9e570d9f8b7b284f852996a4630753c2a
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2024-05-31 21:57:01 +0300

    refactor: responsive design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { FormGroup } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconRegistry } from '@angular/material/icon';
-import { MatStepperModule } from '@angular/material/stepper';
+import { MatStepperModule, StepperOrientation } from '@angular/material/stepper';
+import { map, Observable } from 'rxjs';
 
 import { AddImagesComponent } from './components/add-images/add-images.component';
 import { ApiKeyFormComponent } from './components/api-key-form/api-key-form.component';
@@ -35,27 +37,36 @@ import { SettingsService } from './services/settings.service';
 export class AppComponent implements OnInit {
   addingImages: boolean = false;
   apiKeyFormGroup!: FormGroup;
+  stepperOrientation: Observable<StepperOrientation>;
   
   constructor(
+    private breakpointObserver: BreakpointObserver,
     private cdRef: ChangeDetectorRef,
     public imageListService: ImageListService,
     private matIconReg: MatIconRegistry,
     private ngZone: NgZone,
     public settings: SettingsService
-  ) {}
+  ) {
+    // Observe viewport width so the stepper orientation can be changed
+    // from horizontal to vertical when the viewport width is less than
+    // 800px.
+    this.stepperOrientation = this.breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({matches}) => (matches ? 'horizontal' : 'vertical')));
+  }
 
   ngOnInit() {
-    // Set Angular Material to use the new Material Symbols icon font
+    // Set Angular Material to use the new Material Symbols icon font.
     this.matIconReg.setDefaultFontSetClass('material-symbols-outlined');
   }
 
   setApiKeyFormGroup(formGroup: FormGroup): void {
     // Wrap the updating of the form group in ngZone and manually
     // trigger change detection to avoid
-    // `ExpressionChangedAfterItHasBeenCheckedError`
+    // `ExpressionChangedAfterItHasBeenCheckedError`.
     this.ngZone.run(() => {
       this.apiKeyFormGroup = formGroup;
-      this.cdRef.detectChanges(); // Manually trigger change detection
+      this.cdRef.detectChanges();
     });
   }
 
