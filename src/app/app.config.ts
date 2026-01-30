2663a4fb0ef7f2567ed99a7032e5efb357a4deb0
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2026-01-30 12:59:48 +0200

    wire server to Angular i dev and csrf token support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field'
 
+import { apiCredentialsInterceptor } from './interceptors/api-credentials.interceptor';
+import { apiCsrfInterceptor } from './interceptors/api-csrf.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(
+      withInterceptors([apiCredentialsInterceptor, apiCsrfInterceptor]),
+    ),
     provideRouter(routes),
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
   ]
